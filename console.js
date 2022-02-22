a6e3676c0ec5159e1cb6768cd90c4795f48e1e78
Cristiano Ferreira da Silva <toshiekuri1977@gmail.com>
2022-02-23 05:42:24 +0900

    operadores e funcoes

---
+++ console.js
@@ -0,0 +1,11 @@
+// log -> registro
+
+const minhaVar = true;
+
+console.log("deu erro!");
+//console.log("eu sou um texto");
+//console.log(minhaVar);
+
+// tratamento de erro!
+
+console.error(new Error('deu erro!'));
---
+++ template-strings.js
@@ -0,0 +1,40 @@
+//const nome = "Cris";
+//const idade = 2022-1977;
+//const cidadeDeNascimento = "Taubaté";
+//
+////const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de  " + cidadeDeNascimento;
+//
+//const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade ${cidadeDeNascimento}`;
+//
+//console.log(apresentacao);
+
+//a = []
+//b = 20
+//c = true
+//d = ''
+
+//if (b === 20 && c != false && d == 0) {
+//    console.log('Verdade')
+//} else {
+//    console.log('Falso')
+//}
+
+//if (a == 0) {
+//    console.log('Verdade')
+//} else {
+//    console.log('Falso')
+//}
+
+//if (b == '20') {
+//    console.log('Verdade')
+//}else {
+//    console.log('Falso')
+//}
+
+const nome = "Cristiano";
+const idade = 44;
+const bebidaMaior = "cerveja gelada";
+const bebidaMenor = "suco de uva";
+
+const pedido = `${nome} diz: "por favor, quero beber ${idade >= 18 ? bebidaMaior : bebidaMenor}"`
+console.log(pedido)
